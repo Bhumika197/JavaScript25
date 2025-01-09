c7d99a9b2650452dbf269a982fdb6e907f535d35
Bhumika197 <chintabhumika07@gmail.com>
2025-01-10 00:10:54 +0530

    Basic JS

---
+++ BasicJS.js
@@ -0,0 +1,75 @@
+// // console.log("Hello world")
+// // var keyword 
+// var a =10; 
+// console.log(a)
+
+// //ressignment 
+// a = 20
+// console.log(a)
+//  if(true){
+
+//     var b = 12; 
+//     console.log(b)
+//     // redeclaration
+//     var b = 22 
+//     console.log(b)
+
+//  }
+//  console.log(a)
+//  console.log(b)
+
+// constant Keyword
+// const a = 10; 
+// console.log(a)
+
+// const b = 20; 
+// console.log(b)
+
+// //reasignment of a is not possible 
+// // const a = 20; 
+// // console.log(a)
+
+// if(true){
+
+//    const a = 12;
+//    console.log(a)
+
+//    //const a = 22; 
+//    // console.log(a)
+// }
+
+// console.log(a)
+// console.log(b)
+
+//let Keyword
+ let a = 10; 
+ console.log(a)
+
+ a = 20; 
+ console.log(a)// reassignment is allowed 
+ if(true){
+   let b = 12; 
+   console.log(b)
+   //let b= 11 ;// cannot be redeclared 
+   // console.log(b)
+ }
+console.log(a)
+// console.log(b) //b is out of scope due to let 
+
+// x = 5; 
+// y = "5"; 
+
+// if(x == y){
+//    console.log( True)
+// }else{
+//    console.log( False)
+// }
+
+//  c = 4; 
+//  d = "4";
+//  //strict type of data type checking is done ; 
+//  if (c === d){
+//    console.log(True)
+//  }else{
+//    console.log(False)
+//  }
